faaba876c4bd44c408c2e7fe5357c2f433e7afc3
Mehemmedeli <mehemmedeli678@gmail.com>
2021-12-11 14:23:55 +0400

    ss

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var b=document.querySelector("button");
+var h=document.querySelector("#col");
+var s=document.querySelector("#sol");
+
+b.onclick=function() {
+h.style.color="red"
+s.style.color="red"
+console.log("salam")
+}
